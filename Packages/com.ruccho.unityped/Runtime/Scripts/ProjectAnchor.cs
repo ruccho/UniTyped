585eca6dbc4e2b1574900a558b0d8ee23b4faba9
ruccho <noboroo2000@yahoo.co.jp>
2023-05-04 18:15:36 +0900

    Preserve ProjectAnchor

--- Packages/com.ruccho.unityped/Runtime/Scripts/ProjectAnchor.cs
+++ Packages/com.ruccho.unityped/Runtime/Scripts/ProjectAnchor.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Scripting;
 
 namespace UniTyped
 {
+    [Preserve]
     static class ProjectAnchor
     {
     }
